7219a62c6db4ab1286955205019fce26cbdebc93
georginacostilla <georginacostilla.gc@gmail.com>
2025-05-06 05:03:16 -0300

    Correcciones en el registro

--- src/assets/pages/Register.jsx
+++ src/assets/pages/Register.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
-import "./styleForm.css";
+import './styleForm.css';
+import Swal from "sweetalert2";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -14,8 +15,14 @@ const Register = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
+
     if (password !== repeatPassword) {
-      alert("Las contraseñas no coinciden.");
+      Swal.fire({
+        icon: "warning",
+        title: "Contraseñas no coinciden",
+        text: "Por favor, asegúrate de que ambas contraseñas sean iguales.",
+        confirmButtonText: "Aceptar",
+      });
       return;
     }
 
@@ -26,174 +33,122 @@ const Register = () => {
         email,
         password,
       });
-      alert("¡Registro exitoso! Ahora puedes iniciar sesión.");
-      navigate("/");
+      Swal.fire({
+        icon: "success",
+        title: "Éxito",
+        text: "Registro exitoso. Ahora puedes iniciar sesión.",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+      }).then(() => {
+        navigate("/");
+      });
     } catch (error) {
-      console.error("Error al registrarse:", error);
-      alert("El correo ya está en uso o hubo un error.");
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "El correo ya está en uso o hubo un error.",
+        confirmButtonText: "Aceptar",
+      });
     }
   };
 
   return (
-    <div className="mt-5">
-      <Form className="form" onSubmit={handleRegister}>
-        <p className="title">Registro</p>
-        <p className="message">Regístrate y explora todas las funcionalidades.</p>
+    <>
+      <div className='mt-5'>
+        <Form className="form" onSubmit={handleRegister}>
+          <p className="title">Registro</p>
+          <p className="message">Regístrate y explora todas las funcionalidades.</p>
 
-        <Row className="flex">
-          <Col>
-            <Form.Group controlId="formFirstname">
+          <Row className="flex">
+            <Col>
+              <Form.Group controlId="formFirstname">
+                <Form.Label>
+                  <Form.Control
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                  <span>Nombre</span>
+                </Form.Label>
+              </Form.Group>
+            </Col>
+          </Row>
+
+          <Row className="flex">
+            <Col>
+              <Form.Group controlId="formLastname">
+                <Form.Label>
+                  <Form.Control
+                    type="text"
+                    className="input"
+                    placeholder=""
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                  />
+                  <span>Apellido</span>
+                </Form.Label>
+              </Form.Group>
+            </Col>
+          </Row>
+
+          <Form.Group controlId="formEmail">
+            <Form.Label>
               <Form.Control
-                type="text"
+                type="email"
                 className="input"
                 placeholder=" "
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
-              <span>Nombre</span>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="formLastname">
+              <span>Email</span>
+            </Form.Label>
+          </Form.Group>
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>
               <Form.Control
-                type="text"
+                type="password"
                 className="input"
                 placeholder=" "
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
               />
-              <span>Apellido</span>
-            </Form.Group>
-          </Col>
-        </Row>
+              <span>Contraseña</span>
+            </Form.Label>
+          </Form.Group>
 
-        <Form.Group controlId="formEmail">
-          <Form.Control
-            type="email"
-            className="input"
-            placeholder=" "
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <span>Email</span>
-        </Form.Group>
+          <Form.Group controlId="formConfirmPassword">
+            <Form.Label>
+              <Form.Control
+                type="password"
+                className="input"
+                placeholder=" "
+                value={repeatPassword}
+                onChange={(e) => setRepeatPassword(e.target.value)}
+                required
+              />
+              <span>Repetir contraseña</span>
+            </Form.Label>
+          </Form.Group>
 
-        <Form.Group controlId="formPassword">
-          <Form.Control
-            type="password"
-            className="input"
-            placeholder=" "
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <span>Contraseña</span>
-        </Form.Group>
+          <Button type="submit" className="submit w-100">
+            Registrarse
+          </Button>
 
-        <Form.Group controlId="formConfirmPassword">
-          <Form.Control
-            type="password"
-            className="input"
-            placeholder=" "
-            value={repeatPassword}
-            onChange={(e) => setRepeatPassword(e.target.value)}
-            required
-          />
-          <span>Repetir contraseña</span>
-        </Form.Group>
-
-        <Button type="submit" className="submit w-100">
-          Registrarse
-        </Button>
-
-        <p className="signin">
-          ¿Ya tienes una cuenta? <Link to="/">Inicia sesión</Link>
-        </p>
-      </Form>
-    </div>
+          <p className="signin">
+            ¿Ya tienes una cuenta? <Link to="/">Inicia sesión</Link>
+          </p>
+        </Form>
+      </div>
+    </>
   );
 };
 
 export default Register;
-
-// import { Form, Button, Row, Col } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
-// import './styleForm.css';
-
-// const Register = () => {
-//   return (
-//     <>
-//       <div className='mt-5'>
-//         <Form className="form">
-//           <p className="title">Registro</p>
-//           <p className="message">Regístrate y explora todas las funcionalidades.</p>
-
-//           <Row className="flex">
-//             <Col>
-//               <Form.Group controlId="formFirstname">
-//                 <Form.Label>
-//                   <Form.Control
-//                     type="text"
-//                     className="input"
-//                     placeholder=""
-//                     required
-//                   />
-//                   <span>Nombre</span>
-//                 </Form.Label>
-//               </Form.Group>
-//             </Col>
-//           </Row>
-
-//           <Form.Group controlId="formEmail">
-//             <Form.Label>
-//               <Form.Control
-//                 type="email"
-//                 className="input"
-//                 placeholder=" "
-//                 required
-//               />
-//               <span>Email</span>
-//             </Form.Label>
-//           </Form.Group>
-
-//           <Form.Group controlId="formPassword">
-//             <Form.Label>
-//               <Form.Control
-//                 type="password"
-//                 className="input"
-//                 placeholder=" "
-//                 required
-//               />
-//               <span>Contraseña</span>
-//             </Form.Label>
-//           </Form.Group>
-
-//           <Form.Group controlId="formConfirmPassword">
-//             <Form.Label>
-//               <Form.Control
-//                 type="password"
-//                 className="input"
-//                 placeholder=" "
-//                 required
-//               />
-//               <span>Repetir contraseña</span>
-//             </Form.Label>
-//           </Form.Group>
-
-//           <Button type="submit" className="submit w-100">
-//             Registrarse
-//           </Button>
-
-//           <p className="signin">
-//             ¿Ya tienes una cuenta? <Link to="/">Inicia sesión</Link>
-//           </p>
-//         </Form>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Register;
